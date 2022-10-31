6ab18772aabbe17ad909e7293e8672bde9f8da2d
Koki Shinjo <sktometometo@gmail.com>
2022-10-31 14:14:52 +0900

    add

---
+++ sketchbooks/sample_client/src/main.cpp
@@ -0,0 +1,36 @@
+#define ROSSERIAL_ARDUINO_BLUETOOTH
+
+#include <M5Stack.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+
+ros::NodeHandle nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
+void setup()
+{
+  M5.begin();
+  M5.Lcd.begin();
+  nh.initNode();
+  nh.advertise(chatter);
+
+  M5.Lcd.print("M5Stack rosserial with bluetooth\n");
+
+  while (not nh.connected()) {
+      nh.spinOnce();
+      delay(100);
+  }
+  M5.Lcd.print("Bluetooth connected.");
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(1000);
+}
