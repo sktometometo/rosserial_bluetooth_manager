43b4b82e28cd64407875b14e01f36f218a9958a2
Koki Shinjo <sktometometo@gmail.com>
2022-11-24 15:26:03 +0900

    update rosserial_bluetooth_microphone sample

--- sketchbooks/rosserial_bluetooth_microphone/src/main.cpp
+++ sketchbooks/rosserial_bluetooth_microphone/src/main.cpp
@@ -10,28 +10,41 @@
 #define PIN_CLK  22
 #define PIN_DATA 21
 
-#define I2S_BUFFER_SIZE 512
+#define I2S_BUFFER_SIZE 256
 
 const char sample_format[] = "S16LE";
 const char coding_format[] = "WAVE";
 
 int16_t i2s_samples[I2S_BUFFER_SIZE];
 
-ros::NodeHandle nh;
+ros::NodeHandle_<ArduinoHardware, 10, 10, 2048, 2048> nh;
 audio_common_msgs::AudioData msg_audio_data;
 audio_common_msgs::AudioInfo msg_audio_info;
 ros::Publisher publisher_audio_data("~output/data", &msg_audio_data);
 ros::Publisher publisher_audio_info("~output/info", &msg_audio_info);
 
+uint8_t bt_address[6];
+char bt_address_str[18] = {0};
+
 
 void setup() {
+
+  esp_read_mac(bt_address, ESP_MAC_BT);
+  sprintf(bt_address_str,
+          "%02X:%02X:%02X:%02X:%02X:%02X",
+          bt_address[0],
+          bt_address[1],
+          bt_address[2],
+          bt_address[3],
+          bt_address[4],
+          bt_address[5]);
+
   M5.begin(true, true, true, true);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setTextColor(WHITE, BLACK);
-  M5.Lcd.fillRect(0, 0, 320, 30, BLACK);
-  M5.Lcd.setTextDatum(TC_DATUM);
-  M5.Lcd.drawString("PDM Unit", 160, 3, 4); 
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(BLACK);
+  M5.Lcd.print("ROSSERIAL_BT_MIC\n");
+  M5.Lcd.printf("BT: %s\n", bt_address_str);
 
   i2s_config_t i2s_config = {
       .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
@@ -66,16 +79,20 @@ void setup() {
   msg_audio_info.sample_format = sample_format;
   msg_audio_info.coding_format = coding_format;
 
-  delay(1000);
+  while (not nh.connected()) {
+      delay(1000);
+      nh.spinOnce();
+  }
 }
 
 void read_and_publish() {
     size_t bytes_read;
     i2s_read(I2S_NUM_0, (void *)i2s_samples, sizeof(i2s_samples), &bytes_read, portMAX_DELAY);
-    msg_audio_data.data_length = bytes_read * 2;
+    msg_audio_data.data_length = bytes_read;
     msg_audio_data.data = (uint8_t *)i2s_samples;
     publisher_audio_data.publish(&msg_audio_data);
     publisher_audio_info.publish(&msg_audio_info);
+    nh.spinOnce();
 }
 
 void loop() {
